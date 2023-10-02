f867cdc56b03ca50ced56cf91a9b0801b76ce99b
Bijendra Basnet <bijendra_basnet@apple.com>
2023-10-01 21:21:38 -0700

    hope children favicon

--- hopechf/src/app/layout.js
+++ hopechf/src/app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
       <body className={inter.className}>{children}</body>
     </html>
   )
