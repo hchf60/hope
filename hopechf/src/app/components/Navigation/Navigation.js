3096abcc629b431ed50a33db6f811dd341efcb66
Bijendra Basnet <bijendra_basnet@apple.com>
2023-10-16 00:36:25 -0700

    remove hidden

--- hopechf/src/app/components/Navigation/Navigation.js
+++ hopechf/src/app/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ export default function Navigation() {
  
   return (
     <div>
-      <nav class="bg-gray-800">
+    <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
@@ -14,16 +14,13 @@ export default function Navigation() {
           <p class="rounded-md text-white font-bold font-serif text-2xl text-blue-400 tracking-wide ">HCF</p>
           </Link>
           </div>
-          <div class="hidden md:block">
+          <div class="">
             <div class="ml-10 flex items-baseline space-x-4">
               <Link href="/team" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
               Team
               </Link>
               <Link href="/project" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
               Projects
-              </Link>
-              <Link href="/calendar"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-              Calendar
               </Link>
               <Link href="/reports" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
               Reports
--- hopechf/src/app/page.js
+++ hopechf/src/app/page.js
@@ -13,6 +13,6 @@ export default function Home() {
     </div>
   </main>
   <Footer/>
-</div>
+  </div>
   )
 }
